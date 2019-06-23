7757dec8a1c4c6970f7acb3853e1162c5590a2da
RainlyTree <a1123845350@163.com>
2019-06-23 17:01:45 +0800

    1.模拟实现strncpy 2.模拟实现strncat 3.模拟实现strncmp

---
+++ text_2019_6_21_day_16/text_2019_6_21_day_16/MyStrncmp.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int MyStrncmp(const char *str1, const char *str2, int n)
+{
+	assert(*str1 != NULL && *str2 != NULL);
+	while (*str1 != '\0' && *str2 != '\0' && n != 0)
+	{
+		if (*str1 > *str2)
+			return 1;
+		if (*str1 < *str2)
+			return -1;
+		if (*str1 == *str2)
+		{
+			str1++;
+			str2++;
+		}
+		n--;
+	}
+	return 0;
+}
