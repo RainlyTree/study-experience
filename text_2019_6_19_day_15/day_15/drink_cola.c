787636f17707a8f5c292b1399f8e2b0962c5da36
RainlyTree <a1123845350@163.com>
2019-06-23 16:57:03 +0800

    通讯录

--- text_2019_6_19_day_15/day_15/drink_cola.c
+++ text_2019_6_19_day_15/day_15/drink_cola.c
@@ -1,29 +1,43 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include<stdio.h>
-//#include<string.h>
-//#include<stdlib.h>
-//
-//int main()
-//{
-//	int water = 20;
-//	int count = 0;
-//	int null_water = 0;		//一次喝完漏掉的空瓶子
-//	//count = water;
-//	while (water != 0)
-//	{
-//		count += water;
-//		if (water % 2 == 1)
-//		{
-//			null_water++;
-//		}
-//		if (null_water == 2)
-//		{
-//			count++;
-//			null_water = 1;
-//		}
-//		water = water >> 1;
-//	}
-//	printf("%d", count);
-//	system("pause");
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+	//   法1
+	//int water = 20;
+	//int count = 0;
+	//int null_water = 0;		//一次喝完漏掉的空瓶子
+	////count = water;
+	//while (water != 0)
+	//{
+	//	count += water;
+	//	if (water % 2 == 1)
+	//	{
+	//		null_water++;
+	//	}
+	//	if (null_water == 2)
+	//	{
+	//		count++;
+	//		null_water = 1;
+	//	}
+	//	water = water >> 1;
+	//	printf("%d", count);
+	//}
+
+	//    法二
+	int momey = 20;
+	int empty = 0;
+	int total = 0;
+	total = momey;
+	empty = momey;
+	while (empty >= 2)
+	{
+		total = total + empty/2;
+		empty = empty / 2 + empty % 2;
+	}
+	printf("%d", total);
+	system("pause");
+	return 0;
+}
